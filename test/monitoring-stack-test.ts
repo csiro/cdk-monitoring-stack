18db90377a32ef549ed5db0e3f3bbbcbe3fd938d
Peter Baker <peter.baker122@csiro.au>
2025-08-13 15:06:19 +1000

    Removing testing dependency due to license issues with ts-jest
    Signed-off-by: Peter Baker <peter.baker122@csiro.au>

--- test/monitoring-stack-test.ts
+++
@@ -1,8 +0,0 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-
-// Example test
-
-test("SQS Queue Created", () => {
-  //   const app = new cdk.App();
-});
