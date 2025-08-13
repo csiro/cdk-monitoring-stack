8b7b815abfead6a9fab8226c10220675365a2904
Peter Baker <peter.baker122@csiro.au>
2025-08-13 14:21:49 +1000

    Fixing out
    Signed-off-by: Peter Baker <peter.baker122@csiro.au>

--- out/monitoring-stack-test.ts
+++
@@ -1,9 +0,0 @@
-// Original path: ./test/monitoring-stack-test.ts 
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-
-// Example test
-
-test("SQS Queue Created", () => {
-  //   const app = new cdk.App();
-});
